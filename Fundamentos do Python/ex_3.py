52ff9cd115c93c026eb84f2e09d5c2f8b3b41343
João Lucas <joao.lgomes1166@gmail.com>
2025-08-21 10:58:20 -0300

    Operadores

---
+++ Fundamentos do Python/ex_3.py
@@ -0,0 +1,10 @@
+num1 = 10
+num2 = 5
+
+print(num1 + num2)  # Adição
+print(num1 - num2)  # Subtração
+print(num1 * num2)  # Multiplicação
+print(num1 / num2)  # Divisão
+print(num1 // num2) # Divisão inteira
+print(num1 % num2)  # Módulo
+print(num1 ** num2) # Exponenciação
