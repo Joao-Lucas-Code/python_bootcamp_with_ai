7c227e884f2c7cad47a3f9643310629d95e65d9b
João Lucas <joao.lgomes1166@gmail.com>
2025-08-21 10:40:57 -0300

    Variaveis, Print e Printf

---
+++ Fundamentos do Python/ex_1.py
@@ -0,0 +1,11 @@
+nome = "Joao Lucas"
+idade = 19
+altura = 1.78
+mensagem = "Bom dia!"
+
+print(nome)
+print(idade)
+print(altura)
+print(mensagem)
+
+print(f"Nome: {nome}, Idade: {idade}, Altura: {altura}, Mensagem: {mensagem}")
