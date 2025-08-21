d63dd778a3a9dea4a009397d11d463332fe4875f
João Lucas <joao.lgomes1166@gmail.com>
2025-08-21 10:48:49 -0300

    type

---
+++ Fundamentos do Python/ex_2.py
@@ -0,0 +1,7 @@
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+
+print(f"Nome: {nome}, Sua idade daqui 5 anos sera: {idade + 5}")
+
+print(type(nome))
+print(type(idade))
