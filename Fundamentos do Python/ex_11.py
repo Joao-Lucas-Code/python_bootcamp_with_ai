71b9d4a73724a7145a591cf52446b348f9d5efaa
João Lucas <joao.lgomes1166@gmail.com>
2025-08-22 09:55:47 -0300

    lista

---
+++ Fundamentos do Python/ex_11.py
@@ -0,0 +1,6 @@
+frutas = ['maçã', 'banana', 'laranja']
+
+frutas.append('uva')  # Adiciona 'uva' ao final da lista
+frutas.append('pera')  # Adiciona 'pera' ao final da lista
+frutas.remove('banana')  # Remove 'banana' da lista
+print(frutas)
