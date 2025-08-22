379ddc26d46ef9cfd38d19ddb9a6af2fd259747c
João Lucas <joao.lgomes1166@gmail.com>
2025-08-22 13:41:19 -0300

    class

---
+++ POO/ex_1.py
@@ -0,0 +1,12 @@
+class Casa:
+    def __init__(self, cor, tamanho):
+        self.cor = cor
+        self.tamanho = tamanho
+
+    def descrever(self):
+        return f"Esta casa é {self.cor} e tem {self.tamanho} metros quadrados."
+
+casa1 = Casa("azul", 100)
+casa2 = Casa("vermelha", 150)
+print(casa1.descrever())
+print(casa2.descrever())
