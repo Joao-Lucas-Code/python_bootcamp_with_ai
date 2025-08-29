c2fb2a3999ebc43719d8ae4217e3e421032384d0
João Lucas <joao.lgomes1166@gmail.com>
2025-08-29 10:12:35 -0300

    financiamento

---
+++ Controle de Fluxo/aula_2.py
@@ -0,0 +1,9 @@
+# Logical Operators (Operadores Logicos)
+
+renda_acima_5mil = True
+nome_limpo = False
+
+if renda_acima_5mil and nome_limpo:
+    print("Financiamento aprovado!")
+else:
+    print("Financiamento não aprovado!")
