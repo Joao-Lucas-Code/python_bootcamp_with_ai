2bab9bd4ed911c5af5be2b9474b251cc2e2b36bb
João Lucas <joao.lgomes1166@gmail.com>
2025-08-29 10:14:46 -0300

    emprestimo

---
+++ Controle de Fluxo/aula_3.py
@@ -0,0 +1,8 @@
+# Multiple Comparison Operators (Multiplos Operadores de Comparação)
+idade = 25
+renda = 3000
+
+if idade > 18 and idade < 65 and renda > 2000:
+    print("Você se qualifica para o empréstimo!")
+else:
+    print("Você não se qualifica para o empréstimo.")
