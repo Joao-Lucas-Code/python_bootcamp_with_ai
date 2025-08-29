2f6a00b224cf48957e46e87e40f79d8c9fbbaaee
João Lucas <joao.lgomes1166@gmail.com>
2025-08-29 11:33:02 -0300

    Criando um Retangulo

---
+++ Controle de Fluxo/aula_9.py
@@ -0,0 +1,14 @@
+'''
+Criar um retangulo de 6x6
+* * * * * * 
+* * * * * *
+* * * * * *
+* * * * * *
+* * * * * *
+* * * * * *
+'''
+
+for i in range(6):
+    for j in range(6):
+        print("*", end=" ")
+    print()
