0b03f6d5a668652bc8ac876dedf537913e1cb0a0
João Lucas <joao.lgomes1166@gmail.com>
2025-08-29 11:10:51 -0300

    compra_confirmada

---
+++ Controle de Fluxo/aula_6.py
@@ -0,0 +1,9 @@
+# Enviar um email com os detalhes da compra online (maximo 3 itens)
+# para compras confirmadas
+
+compra_confirmada = True
+
+if compra_confirmada:
+    print("Email enviado com os detalhes da compra.")
+else:
+    print("Compra não confirmada.")
