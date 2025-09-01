9ba367c98b8dd51ba83a4b7cbbb4901e39d5ddeb
João Lucas <joao.lgomes1166@gmail.com>
2025-09-01 09:03:27 -0300

    functions

---
+++ Functions/ex_01.py
@@ -0,0 +1,9 @@
+# Functions (Funções)
+  # DRY (Don't Repeat Yourself)
+
+def boas_vindas(nome):
+    print(f"Seja bem-vindo(a), {nome}!")
+
+boas_vindas("Alice")
+boas_vindas("Bob")
+boas_vindas("Charlie")
