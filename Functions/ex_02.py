9fad070301d8625a9b26c1b1d4ff9ffef913d54e
João Lucas <joao.lgomes1166@gmail.com>
2025-09-01 09:28:01 -0300

    somar dois numeros

---
+++ Functions/ex_02.py
@@ -0,0 +1,11 @@
+# Functions (Funções)
+  # DRY (Don't Repeat Yourself)
+
+def somar_dois_numeros(a, b):
+    return a + b
+
+resultado1 = somar_dois_numeros(3, 5)
+resultado2 = somar_dois_numeros(10, 20)
+
+print(resultado1)
+print(resultado2)
