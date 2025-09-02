bfe95949372db19ec006862bfc7c2a54f1fa7d82
João Lucas <joao.lgomes1166@gmail.com>
2025-09-02 10:15:09 -0300

    append | remove | pop

---
+++ Listas/ex_03.py
@@ -0,0 +1,17 @@
+# Listas
+  # Estruturas de dados que armazenam coleções de elementos
+  # Manter a sequencia dos dados em uma variável
+
+cidades = ['São Paulo', 'Rio de Janeiro', 'Belo Horizonte']
+
+cidades.append('Curitiba')  # Adiciona um elemento ao final da lista
+
+print(cidades)
+
+cidades.remove('Rio de Janeiro')  # Remove um elemento específico da lista
+
+print(cidades)
+
+cidades.pop(0)
+
+print(cidades)
