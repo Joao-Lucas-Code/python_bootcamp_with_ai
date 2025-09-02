7d9aec3371b44cd26551df7894feccc49e9c0686
João Lucas <joao.lgomes1166@gmail.com>
2025-09-02 10:18:03 -0300

    extend

---
+++ Listas/ex_04.py
@@ -0,0 +1,10 @@
+# Listas
+  # Estruturas de dados que armazenam coleções de elementos
+  # Manter a sequencia dos dados em uma variável
+
+numeros = [1, 2, 3, 4, 5]
+letras = ['a', 'b', 'c', 'd', 'e']
+
+numeros.extend(letras)  # Adiciona os elementos de uma lista a outra
+
+print(numeros)  
