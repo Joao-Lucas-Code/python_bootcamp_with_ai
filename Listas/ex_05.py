5d768bc5a64aed689d1c1ad75b196f9ae848117f
João Lucas <joao.lgomes1166@gmail.com>
2025-09-02 10:19:55 -0300

    concatenando listas

---
+++ Listas/ex_05.py
@@ -0,0 +1,9 @@
+# Listas
+  # Estruturas de dados que armazenam coleções de elementos
+  # Manter a sequencia dos dados em uma variável
+
+itens = [['item1', 'item2'], ['item3', 'item4']]
+
+print(itens)
+print(itens[0][1])  # Acessa o segundo elemento da primeira lista
+print(itens[1][0])  # Acessa o primeiro elemento da segunda lista
