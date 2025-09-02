eea6348f0a137e720cbf026c4074f1b706aaba9f
João Lucas <joao.lgomes1166@gmail.com>
2025-09-02 11:13:08 -0300

    tuples

---
+++ Estrutura de Dados/Tuples/ex_01.py
@@ -0,0 +1,9 @@
+# Tuples
+  # Armazenar mais de uma informação em vaiáveis
+  # Manter a sequencia dos dados em uma variável
+  # Não podem ser alteradas (Immutable)
+
+cores_list = ['amarelo', 'verde', 'azul', 'vermelho']
+cores_tuple = ('amarelo', 'verde', 'azul', 'vermelho')  
+
+cores_tuple.append('roxo')  # AttributeError: 'tuple' object has no attribute 'append'
