20137309e4eaa6fb3b9a7d2f087e7e86a4854260
João Lucas <joao.lgomes1166@gmail.com>
2025-09-02 09:45:13 -0300

    listas

---
+++ Listas/ex_01.py
@@ -0,0 +1,11 @@
+# Listas
+  # Estruturas de dados que armazenam coleções de elementos
+  # Manter a sequencia dos dados em uma variável
+
+cidade1 = 'Rio de Janeiro'
+cidade2 = 'São Paulo'
+cidade3 = 'Belo Horizonte'
+
+cidades = [cidade1, cidade2, cidade3]
+
+print(cidades)
