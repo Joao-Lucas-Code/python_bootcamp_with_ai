973ceabdeb4abb02250057d779367074203f647e
João Lucas <joao.lgomes1166@gmail.com>
2025-09-02 13:48:26 -0300

    array

---
+++ Estrutura de Dados/Array/ex_01.py
@@ -0,0 +1,13 @@
+from array import array
+
+# Arrays (Matriz)
+  # Similar a listas
+  # Melhor perfomace
+
+letras = array('u', ['v', 'x', 'y', 'z'])
+numeros = array('i', [1, 2, 3, 4, 5])
+numeros_float = array('f', [1.0, 2.0, 3.0, 4.0, 5.0])
+
+print(letras)
+print(numeros)
+print(numeros_float)
