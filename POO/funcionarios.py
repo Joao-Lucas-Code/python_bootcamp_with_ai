8daf827395fdb542aebee19cc8fe349bb3c6265e
João Lucas <joao.lgomes1166@gmail.com>
2025-09-04 11:06:02 -0300

    funcionarios

---
+++ POO/funcionarios.py
@@ -0,0 +1,20 @@
+class Funcionarios:
+  pass
+
+usuario1 = Funcionarios()
+usuario2 = Funcionarios()
+
+usuario1.nome = 'João'
+usuario1.sobrenome = 'Silva'
+usuario1.idade = 30
+
+usuario2.nome = 'Maria'
+usuario2.sobrenome = 'Souza'
+usuario2.idade = 25
+
+print(usuario1.nome)      # Saída: João
+print(usuario1.sobrenome)  # Saída: Silva
+print(usuario1.idade)     # Saída: 30
+print(usuario2.nome)      # Saída: Maria
+print(usuario2.sobrenome)  # Saída: Souza
+print(usuario2.idade)     # Saída: 25
