027479aa62475adf87136b7b761cf5708019b10b
João Lucas <joao.lgomes1166@gmail.com>
2025-09-10 11:40:05 -0300

    desafio4

---
+++ Desafios/desafio_4.py
@@ -0,0 +1,5 @@
+first_name = str(input("Digite seu primeiro nome: "))
+last_name = str(input("Digite seu sobrenome: "))
+age = int(input("Digite sua idade: "))
+
+print(f"Olá, {first_name} {last_name}! Você tem {age} anos.")
