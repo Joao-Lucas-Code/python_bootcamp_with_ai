8eeb44b239e9e4eeba823463bc00080d25e6af39
João Lucas <joao.lgomes1166@gmail.com>
2025-09-12 11:29:57 -0300

    desafio 14

--- Desafios/desafio_14.py
+++ Desafios/desafio_14.py
@@ -1,10 +1,16 @@
 from time import sleep
 
+print("Agora com break:")
 for i in range(1, 11):
     sleep(0.25)
     if i == 5:
+        sleep(0.25)
+        print("Interrompido na 5ª iteração")
         break
     print(i)
+sleep(0.5)
+
+print("Agora com continue:")
 for i in range(1, 11):
     sleep(0.25)
     if i == 5:
