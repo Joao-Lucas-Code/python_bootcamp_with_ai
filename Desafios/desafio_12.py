30650d4266c12efe9e2df3d09a2accd4033387af
João Lucas <joao.lgomes1166@gmail.com>
2025-09-12 11:16:05 -0300

    desafio12

--- Desafios/desafio_12.py
+++ Desafios/desafio_12.py
@@ -1,1 +1,7 @@
+frutas = ["maçã", "banana", "laranja", "uva", "manga"]
 
+vegetais = ["alface", "tomate", "cenoura", "pepino", "espinafre"]
+
+for fruta in frutas:
+    for vegetal in vegetais:
+        print(f"Eu gosto de {fruta} e {vegetal}")
