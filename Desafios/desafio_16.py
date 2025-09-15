f2002285f8bad8b1d5a2119956510d0859795318
João Lucas <joao.lgomes1166@gmail.com>
2025-09-15 09:57:13 -0300

    desafio16

---
+++ Desafios/desafio_16.py
@@ -0,0 +1,7 @@
+while True:
+    numero = int(input('Digite um número: '))
+    if numero > 10:
+        print(f'O número {numero} é maior que 10.')
+        break
+    else:
+        print(f'O número {numero} é menor ou igual a 10.')
