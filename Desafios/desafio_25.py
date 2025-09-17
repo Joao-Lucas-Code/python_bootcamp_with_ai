64db30cb67e2f2c5182060475357587a9233e89e
João Lucas <joao.lgomes1166@gmail.com>
2025-09-17 12:18:55 -0300

    desafio25

---
+++ Desafios/desafio_25.py
@@ -0,0 +1,6 @@
+def soma(num1, num2):
+    return num1 + num2
+
+n1 = float(input("Digite o primeiro número: "))
+n2 = float(input("Digite o segundo número: "))
+print(f"A soma de {n1} e {n2} é {soma(n1, n2)}.")
