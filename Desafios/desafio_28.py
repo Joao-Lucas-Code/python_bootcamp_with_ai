967a5ebfc0fae24bba651db34a8266ed14604744
João Lucas <joao.lgomes1166@gmail.com>
2025-09-18 09:49:04 -0300

    desafio28

---
+++ Desafios/desafio_28.py
@@ -0,0 +1,9 @@
+def dobro(n):
+    return n * 2
+
+def quadrado(n):
+    return n ** 2
+
+num1 = float(input("Digite um número: "))
+print(f"O dobro de {num1} é {dobro(num1)}.")
+print(f"O quadrado de {num1} é {quadrado(num1)}.")
