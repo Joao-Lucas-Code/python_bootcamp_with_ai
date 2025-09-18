74470605e4610a14a7e57c3916489b2fe8be465a
João Lucas <joao.lgomes1166@gmail.com>
2025-09-18 09:44:51 -0300

    desafio27

---
+++ Desafios/desafio_27.py
@@ -0,0 +1,8 @@
+def fatorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * fatorial(n - 1)
+    
+num = int(input("Digite um número para calcular o fatorial: "))
+print(f"O fatorial de {num} é {fatorial(num)}.")
