d03f2922d33f8ccdd00ed53fbea8f9c3ddf1cc81
João Lucas <joao.lgomes1166@gmail.com>
2025-09-18 09:55:00 -0300

    desafio30

--- Desafios/desafio_30.py
+++ Desafios/desafio_30.py
@@ -2,4 +2,5 @@ multiplicação = lambda x, y: x * y
 
 num1 = float(input("Digite o primeiro número: "))
 num2 = float(input("Digite o segundo número: "))
+
 print(f"A multiplicação de {num1} e {num2} é {multiplicação(num1, num2)}.")
