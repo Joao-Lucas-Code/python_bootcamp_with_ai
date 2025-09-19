fc94736104c84a4b840df8cf1409cf59599b8580
João Lucas <joao.lgomes1166@gmail.com>
2025-09-19 10:45:10 -0300

    desafio34

---
+++ Desafios/desafio_34.py
@@ -0,0 +1,5 @@
+soma_quadrado = lambda x, y: x**2 + y**2
+
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+print(f"A soma dos quadrados de {num1} e {num2} é {soma_quadrado(num1, num2)}.")
