e761fcabf7a32d52d519407e01a1d3386f665ae9
João Lucas <joao.lgomes1166@gmail.com>
2025-09-23 09:41:50 -0300

    openpyxl

--- Data Science/fifa_2.py
+++ Data Science/fifa_2.py
@@ -1,7 +1,10 @@
 import pandas as pd
 
+
 df = pd.read_csv('c:/Users/João Lucas/Cursos/Bootcamp_Python_IA/Data Science/fifa.csv')
 
 df['Total'] = df['Overall'] + df['Potential']
 
 print(df[['Name', 'Overall', 'Potential', 'Total']])
+
+df.to_excel('fifa_novo.xlsx', index=False)
