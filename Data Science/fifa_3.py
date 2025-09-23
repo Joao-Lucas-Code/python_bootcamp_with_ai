6d0b4c733f7345006c36f7cb63211843266ee266
João Lucas <joao.lgomes1166@gmail.com>
2025-09-23 09:46:36 -0300

    fifa_new

--- Data Science/fifa_3.py
+++ Data Science/fifa_3.py
@@ -2,3 +2,7 @@ import pandas as pd
 
 
 df = pd.read_csv('c:/Users/João Lucas/Cursos/Bootcamp_Python_IA/Data Science/fifa.csv')
+
+dados = df['Total'] = df['Overall'] + df['Potential']
+
+dados.to_csv('fifa_novo.csv', index=False, sep='\t')
