dce59cc713f5aba83791acdba17540f9d8dfa7b1
João Lucas <joao.lgomes1166@gmail.com>
2025-09-23 09:24:50 -0300

    noventa_mais

--- Data Science/fifa.py
+++ Data Science/fifa.py
@@ -2,4 +2,8 @@ import pandas as pd
 
 df = pd.read_csv('c:/Users/João Lucas/Cursos/Bootcamp_Python_IA/Data Science/fifa.csv')
 
-print(df.iloc[2,2])
+# print(df.iloc[2,2])
+
+noventa_mais = df.loc[df['Overall'] > 90, ['Name', 'Overall', 'Club']]  # Jogadores com overall maior que 90
+
+print(noventa_mais)
