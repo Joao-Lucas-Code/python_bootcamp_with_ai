f5455218d07b38d58fc560a8b7f14a4b7f6d920b
João Lucas <joao.lgomes1166@gmail.com>
2025-09-29 10:51:42 -0300

    conditions

--- Data Science/pokemon.py
+++ Data Science/pokemon.py
@@ -3,5 +3,8 @@ df = pd.read_csv('c:/Users/João Lucas/Cursos/Bootcamp_Python_IA/Data Science/po
 
 filter1 = df['generation'] == 1
 
+filter2 = df['type1'] == 'Grass'
+filter3 = df['type2'] == 'Poison'
 
-print(df[filter1])
+
+print(df[filter2 & filter3])
